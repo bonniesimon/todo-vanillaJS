c5537e607cf4c11cf7110aef1da8c2b0141f91b5
Bonnie Simon <bonniesimon@protonmail.com>
2019-08-20 23:07:58 +0530

    Added nav-bar using materializecss

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const form = document.querySelector('#task-form');
 const taskInput = document.querySelector('#task');
 const submitBtn = document.querySelector('#submit');
 const collection = document.querySelector('.collection');
-
+const filterInput = document.querySelector('#filter');
+const clearBtn = document.querySelector('.clear-task');
 
 
 
@@ -23,7 +24,7 @@ function loadEventListners(){
             alert('Input Task, not emptiness!! xD');
         }else{
             let item = taskInput.value;
-            console.log(item);
+            // console.log(item);
             const li = document.createElement('li');
             li.className='collection-item';
             li.appendChild(document.createTextNode(item));
@@ -38,19 +39,59 @@ function loadEventListners(){
         e.preventDefault();
     });
 
+    //Removing Tasks
     collection.addEventListener('click' , (e)=>{
-        console.log(e.target);
+        // console.log(e.target);
         if(e.target.parentElement.classList.contains('delete-item') || e.target.classList.contains('delete-item') ){
             e.target.parentElement.parentElement.remove();
-           
+            
         }
-        
-        
+    });
+
+    //Clear Tasks
+    clearBtn.addEventListener('click' , (e) =>{
+        while(collection.firstChild){
+            collection.removeChild(collection.firstChild);
+        }
     })
-}
+     
+
+    //Filter Tasks
+    filterInput.addEventListener('keyup' , (e)=>{
+        const text = e.target.value.toLowerCase();
+
+        document.querySelectorAll('.collection-item').forEach((item)=>{
+           if(item.firstChild.textContent.toLowerCase().indexOf(text) != -1){
+                item.style.display='block';
+           } else{
+                item.style.display = 'none';
+           }
+        })
+    });
+
+
+
+} /* loadEventListners END*/
+
+
+
+//Working of indexOf():
+
+// let word = 'longestwordever!';
+// let subString = 'long';
+// let output = word.toLowerCase().indexOf(subString);
+// console.log(output);
 
 
 
 
+
+/* 
+THINGS I LEARNED:
+
+-how to remove items properly in a todo list
+-how to filter through a list of items using indexOf();
+-how to clear out all tasks using firstChild and removeChild
+*/
 
 
